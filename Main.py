6c0262a0a72650f02fc2e7ef8c942db150dd5924
JStoresund <johanstore2002@gmail.com>
2024-10-20 13:26:59 +0200

    luk

--- Main.py
+++ Main.py
@@ -4,4 +4,12 @@ def Johan_kul(n=print('kul',end='')):
 
 Johan_kul()
 
+def Thomas_kul(n=print(end="luk")):
+    if not n==None:
+        print(n)
+        return
+    print("luk")
+
+Thomas_kul()
+
     
